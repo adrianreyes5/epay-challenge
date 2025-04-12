62faedef8bc804eef456e7e0340ef9bfaa643198
Adrian Reyes <adriandmc05@gmail.com>
2025-04-12 11:33:07 -0400

    feat: add client route

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.API_GATEWAY_PORT || 3000;
+// Routes
+app.use("/api/clients", require("./routes/client.routes"));
+
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`API Gateway Service running on port ${PORT}`);
 });
