2a0f925c06041a1280ed90f0e2ef2f57c12c1d14
Adrian Reyes <adriandmc05@gmail.com>
2025-04-13 14:33:19 -0400

    delete outside folders

--- api-db/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.use("/clients", require("./client.routes"));
-router.use("/wallets", require("./wallet.routes"));
-router.use("/payments", require("./payment.routes"));
-
-module.exports = router;
